efe14833d57b592f905ac409de536cd727d48a01
Ahmet Sina Ustem <asend.ustem@gmail.com>
2017-02-25 23:33:53 +0300

    Get Current Location

--- assets/js/map.js
+++ assets/js/map.js
@@ -7,10 +7,12 @@ $.getJSON('data.json',function (data) {
 function initMap() {
     map = new google.maps.Map(document.getElementById("map"),{
         center: { lat:-34.397, lng:150.644},
-        zoom:0,
+        zoom:7,
         mapTypeControl: false,
         streetViewControl: false
     });
+
+    getCurrentLocation();
 
     var stores = window.json.stores;
     console.log(stores.length);
@@ -33,6 +35,26 @@ function initMap() {
 
 }
 
+function getCurrentLocation() {
+    var infoWindow = new google.maps.InfoWindow({map: map});
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            map.setCenter(pos);
+        }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+}
 
 function attachInfoToMarker(marker, Info){
     var infoWindow = new google.maps.InfoWindow({
@@ -42,4 +64,11 @@ function attachInfoToMarker(marker, Info){
     marker.addListener('click', function () {
         infoWindow.open(marker.get('map'), marker);
     });
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
 }
